63bb2ed03cc9ecbae0befa019ba5df978eafcb89
Jimena Miramontes <jimemiramontes@gmail.com>
2026-05-07 13:40:38 -0300

    Modificacion texto boton

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -2,7 +2,7 @@ import { Item } from "../Item/Item";
 export const ItemDetail = ({ item }) => {
   return (
     <Item {...item}>
-      <button className="btn">Agregar al carrito</button>
+      <button className="btn">Quiero sumarme</button>
     </Item>
   );
 };
